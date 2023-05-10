e6f4b7062d74f792761045dfb66ac624b6edcc98
Stevenrej <steveo732@gmail.com>
2023-05-09 21:24:54 -0500

    first commit

---
+++ emailRoutes.js
@@ -0,0 +1,40 @@
+const Mailgun = require('mailgun-js');
+
+const mailgun = new Mailgun({ apiKey: process.env.MAILGUN_KEY, domain: process.env.MAILGUN_DOMAIN });
+
+async function sendEmail(req, res) {
+  try {
+    let { name, email, message } = req.body;
+
+    const msg = {
+      to: 'robertwilsonfornari98@gmail.com',
+      from: 'message@personalportfolio.net',
+      subject: 'Message from ' + name,
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Message: ${message}`,
+      html: `
+        <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Message: ${message}</p>
+      `,
+    };
+    
+
+    mailgun.messages().send(msg, (error, body) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send({ message: 'An error occurred' });
+      } else {
+        res.send({ message: 'Email sent successfully', msg });
+        console.log(msg.text);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error sending email' });
+  }
+}
+
+module.exports = { sendEmail };
